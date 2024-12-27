4167bb4184830c5f983ee9b3e50de4c3119089cb
david-pessoa <pessoa.dvlbp@gmail.com>
2024-12-27 18:35:24 -0300

    cria staticfiles

--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from core.models import Funcionario, Servico
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-class TesteView(TemplateView):
-    template_name = '404.html'
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['funcionarios'] = Funcionario.objects.all()
+        context['servicos'] = Servico.objects.all()
+        return context
 
